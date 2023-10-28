8e34fef486737253a973451206a8b1dcfd4eaf98
jayveeroda <rodajohnvincent35@gmail.com>
2023-10-28 18:33:59 +0800

    enrollment sa college need pa og function ang next og previous tapos signature pad pod

---
+++ enrollment_proj/src/app/pages/enroll-college2/enroll-college2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enroll-college2',
+  templateUrl: './enroll-college2.component.html',
+  styleUrls: ['./enroll-college2.component.css']
+})
+export class EnrollCollege2Component {
+
+}
