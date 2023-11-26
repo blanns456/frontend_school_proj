7ec238eb21c0e20da36fab95b28ef35c3312b9ec
JBHtzy <jamesbadang16@gmail.com>
2023-11-26 19:30:54 +0800

    accounting finance updated, view user

--- enrollment_proj/src/app/pages/accounting-pov/accounting-finance/accounting-matriculation/accounting-matriculation.component.ts
+++ enrollment_proj/src/app/pages/accounting-pov/accounting-finance/accounting-matriculation/accounting-matriculation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+// import DataTable from 'datatables.net-dt';
+// declare var $: any;
 
 @Component({
   selector: 'app-accounting-matriculation',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./accounting-matriculation.component.css']
 })
 export class AccountingMatriculationComponent {
+  // @ViewChild('dataTable',{static: false}) table:any;
+  // dataTable: any;
+  // constructor() {
+    
+  // }
+
+  // ngAfterViewInit(): void {
+  //   this.dataTable = $(this.table.nativeElement);
+  //   new DataTable(this.dataTable);
+  // }
+
+  // ngOnInit(): void {
+    
+  // }
 
 }
