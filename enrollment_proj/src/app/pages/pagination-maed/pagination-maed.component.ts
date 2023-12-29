2e52e94cbe2418b191f1d4c12b23b1ac13fe15df
JBHtzy <jamesbadang16@gmail.com>
2023-12-29 12:57:15 +0800

    enroll-maed added values

--- enrollment_proj/src/app/pages/pagination-maed/pagination-maed.component.ts
+++ enrollment_proj/src/app/pages/pagination-maed/pagination-maed.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination-maed',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination-maed.component.css']
 })
 export class PaginationMaedComponent {
+  @Input() currentPage!: number;
+  @Input() totalPages!: number;
+  @Output() pageChanged = new EventEmitter<number>();
 
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, index) => index + 1);
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.pageChanged.emit(page);
+    }
+  }
+  onPageChanged(page: number) {
+    this.currentPage = page;
+    alert(this.currentPage);
+    // Fetch data for the new page or update your data as needed
+  }
 }
