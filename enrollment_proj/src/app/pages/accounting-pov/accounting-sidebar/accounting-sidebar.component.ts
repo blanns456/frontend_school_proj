07ae5cae94a50d77b94a2931b50a3e6deabb28aa
JBHtzy <jamesbadang16@gmail.com>
2024-01-13 03:05:15 -0800

    replacing some contents

--- enrollment_proj/src/app/pages/accounting-pov/accounting-sidebar/accounting-sidebar.component.ts
+++ enrollment_proj/src/app/pages/accounting-pov/accounting-sidebar/accounting-sidebar.component.ts
@@ -1,55 +1,58 @@
 import { Component } from '@angular/core';
-import * as $ from "jquery"; // kini sa pgdeclare nis $
+import * as $ from 'jquery'; // kini sa pgdeclare nis $
 
 @Component({
-    selector: 'app-accounting-sidebar',
-    templateUrl: './accounting-sidebar.component.html',
-    styleUrls: ['./accounting-sidebar.component.css']
+  selector: 'app-accounting-sidebar',
+  templateUrl: './accounting-sidebar.component.html',
+  styleUrls: ['./accounting-sidebar.component.css'],
 })
 export class AccountingSidebarComponent {
-    ngOnInit(): void {
-        $(document).ready(function () {
-            $('.menu-item').click(function () {
-                $('.menu-item').removeClass('active');
-                $(this).addClass('active');
-            });
+  ngOnInit(): void {
+    $(document).ready(function () {
+      $('.menu-item').click(function () {
+        $('.menu-item').removeClass('active');
+        $(this).addClass('active');
+      });
 
-            let accSetup = 0;
-            $('#accSetupToggle').click(function () {
-                accSetup++;
-                if (accSetup % 2 == 0) {
-                    $('.accSetupToggle-sub').slideToggle();
-                } else {
-                    $('.accSetupToggle-sub').slideDown();
-                }
-            });
-            let accReport = 0;
-            $('#accReportsToggle').click(function () {
-                accReport++;
-                if (accReport % 2 == 0) {
-                    $('.accReportsToggle-sub').slideToggle();
-                } else {
-                    $('.accReportsToggle-sub').slideDown();
-                }
-            });
-            let accFinance = 0;
-            $('#accFinanceToggle').click(function () {
-                accFinance++;
-                if (accFinance % 2 == 0) {
-                    $('.accFinanceToggle-sub').slideToggle();
-                } else {
-                    $('.accFinanceToggle-sub').slideDown();
-                }
-            });
-            let tellering = 0;
-            $('#telleringToggle').click(function () {
-                tellering++;
-                if (tellering % 2 == 0) {
-                    $('.telleringToggle-sub').slideToggle();
-                } else {
-                    $('.telleringToggle-sub').slideDown();
-                }
-            });
-        })
-    }
+      let accSetup = 0;
+      $('#accSetupToggle').click(function () {
+        accSetup++;
+        if (accSetup % 2 == 0) {
+          $('.accSetupToggle-sub').slideToggle();
+        } else {
+          $('.accSetupToggle-sub').slideDown();
+        }
+      });
+
+      let accReport = 0;
+      $('#accReportsToggle').click(function () {
+        accReport++;
+        if (accReport % 2 == 0) {
+          $('.accReportsToggle-sub').slideToggle();
+        } else {
+          $('.accReportsToggle-sub').slideDown();
+        }
+      });
+
+      let accFinance = 0;
+      $('#accFinanceToggle').click(function () {
+        accFinance++;
+        if (accFinance % 2 == 0) {
+          $('.accFinanceToggle-sub').slideToggle();
+        } else {
+          $('.accFinanceToggle-sub').slideDown();
+        }
+      });
+
+      let tellering = 0;
+      $('#telleringToggle').click(function () {
+        tellering++;
+        if (tellering % 2 == 0) {
+          $('.telleringToggle-sub').slideToggle();
+        } else {
+          $('.telleringToggle-sub').slideDown();
+        }
+      });
+    });
+  }
 }
